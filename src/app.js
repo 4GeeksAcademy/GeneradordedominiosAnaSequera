6cd5aef63885651e43ea5fc0e265039c40fb69b9
Ana Sequera <147461093+AnaSequera@users.noreply.github.com>
2025-05-05 20:10:35 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io'];
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-};
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let ext of extensions) {
+        console.log(p + a + n + ext);
+      }
+    }
+  }
+}
+}
